dc44043fdc79bd0b348fe3c8b91bdcb4bcdcde1e
Kyle Howard <kyle.howard@smsja.net>
2020-05-22 09:17:43 -0500

    adds modals to story and task

--- carpe-diem/carpe-diem-client/src/components/kanban_board/subcomponents/Story.js
+++ carpe-diem/carpe-diem-client/src/components/kanban_board/subcomponents/Story.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../../common/Modal';
+import StoryDetails from './StoryDetails';
 
-const Story = (props) => <span class={props.className}>{props.story.name}</span>
+
+const Story = (props) => {
+
+    const [state, updateState] = useState({ showModal: false })
+    const showDetails = (e) => {
+        updateState((state) => ({ showModal: !state.showModal }))
+    }
+
+    return (
+        <div onClick={showDetails}>
+            <span class={props.className}>{props.story.name}</span>
+            <Modal visible={state.showModal} className="modal" ><StoryDetails story={props.story}></StoryDetails></Modal>
+        </div>)
+}
 
 export default Story;
--- carpe-diem/carpe-diem-client/src/components/kanban_board/subcomponents/Task.js
+++ carpe-diem/carpe-diem-client/src/components/kanban_board/subcomponents/Task.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../../common/Modal';
+import Details from '../../common/Details'
 
-const Task = (props) => (
-    <div class={props.className}>
-        <span> {props.taskStory}</span>
-        <span>{props.taskDetails}</span>
-    </div>)
+
+
+const Task = (props) => {
+    const [state, updateState] = useState({ showModal: false })
+    const showDetails = (e) => {
+        updateState((state) => ({ showModal: !state.showModal }))
+    }
+    return (
+        <div onClick={showDetails} class={props.className}>
+            <span> {props.taskStory}</span>
+            <span>{props.taskDetails}</span>
+            <Modal visible={state.showModal} className="modal" ><Details title={props.taskStory}>{props.taskDetails}</Details></Modal>
+
+        </div>)
+}
 
 export default Task;
