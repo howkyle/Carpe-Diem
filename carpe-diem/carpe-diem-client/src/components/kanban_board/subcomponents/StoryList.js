8765439344e3a8b58023acea4245388258cc87b6
Kyle Howard <kyle.howard@smsja.net>
2020-05-22 09:20:14 -0500

    adds return

--- carpe-diem/carpe-diem-client/src/components/kanban_board/subcomponents/StoryList.js
+++ carpe-diem/carpe-diem-client/src/components/kanban_board/subcomponents/StoryList.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Story from './Story';
 
-const StoryList = (props) => (
-    <div class={props.className}>
-        <div class="storylist-header">{props.children}</div>
-        <div class="storylist-stories">
-            {props.stories.map(story => <Story key={story.id} story={story} className="storylist-story"></Story>)}
-        </div>
+const StoryList = (props) => {
 
-    </div>);
+
+    return (
+        <div class={props.className}>
+            <div class="storylist-header">{props.children}</div>
+            <div class="storylist-stories">
+                {props.stories.map(story => <Story key={story.id} story={story} className="storylist-story"></Story>)}
+            </div>
+        </div>
+    )
+};
 
 
 export default StoryList;
