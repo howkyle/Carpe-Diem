9eb5619bd195c468d59e8ee147b422dd383ab61a
Kyle Howard <kyle.howard@smsja.net>
2020-05-28 20:01:21 -0500

    passes token to rest of service methods

--- carpe-diem/carpe-diem-server/pivotal/api/service.js
+++ carpe-diem/carpe-diem-server/pivotal/api/service.js
@@ -12,7 +12,7 @@ export const getUsers = (token, projectid) => {
 
 }
 
-export const getStories = (projectid) => {
+export const getStories = (token, projectid) => {
     let url = `https://www.pivotaltracker.com/services/v5/projects/${projectid}/stories?fields=tasks,owned_by,name,current_state`
 
     return axios.get(url, { headers: { 'X-TrackerToken': token } })
@@ -21,7 +21,7 @@ export const getStories = (projectid) => {
 
 }
 
-export const getCurrentIteration = (projectid) => {
+export const getCurrentIteration = (token, projectid) => {
     let url = `https://www.pivotaltracker.com/services/v5/projects/${projectid}/iterations?fields=stories(tasks,owned_by(id),name,current_state,description)&scope=current`
 
     return axios.get(url, { headers: { 'X-TrackerToken': token } })
